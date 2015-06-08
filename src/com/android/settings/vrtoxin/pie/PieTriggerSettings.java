b4bc5efdbb3ccff7dcf563d3180c3867d6281034
rogersb11 <brettrogers11@gmail.com>
2015-06-07 23:57:23 -0400

    SlimPie default right side 1/2
    Conflicts:
    	src/com/android/settings/vrtoxin/pie/PieTriggerSettings.java

--- src/com/android/settings/vrtoxin/pie/PieTriggerSettings.java
+++ src/com/android/settings/vrtoxin/pie/PieTriggerSettings.java
@@ -40,8 +40,8 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
 
     private static final int DLG_WARNING = 0;
 
-    // This equals EdgeGesturePosition.LEFT.FLAG
-    private static final int DEFAULT_POSITION = 1 << 0;
+    // This equals EdgeGesturePosition.RIGHT.FLAG
+    private static final int DEFAULT_POSITION = 2 << 0;
 
     private static final String PREF_PIE_DISABLE_IME_TRIGGERS = "pie_disable_ime_triggers";
     private static final String PIE_TRIGGER_SENSITIVITY = "pie_trigger_sensitivity";
