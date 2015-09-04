b3b9969ed80d84a292befd3c2fd29ab5f2666e1e
rogersb11 <brettrogers11@gmail.com>
2015-09-04 04:12:20 -0400

    Settings:Status bar carrier label font size 1/2

--- src/com/android/settings/vrtoxin/CarrierLabel.java
+++ src/com/android/settings/vrtoxin/CarrierLabel.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 DarkKat
+ * Copyright (C) 2015 The VRToxin Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +17,11 @@
 
 package com.android.settings.vrtoxin;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
@@ -25,10 +30,15 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.vrtoxin.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -49,9 +59,14 @@ public class CarrierLabel extends SettingsPreferenceFragment implements
             "carrier_label_number_of_notification_icons";
     private static final String PREF_COLOR =
             "carrier_label_color";
+    private static final String STATUS_BAR_CARRIER_FONT_SIZE  =
+            "status_bar_carrier_font_size";
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
 
     private static final int WHITE = 0xffffffff;
-    private static final int HOLO_BLUE_LIGHT = 0xff33b5e5;
+    private static final int VRTOXIN_BLUE = 0xff1976D2;
 
     private SwitchPreference mShow;
     private SwitchPreference mShowOnLockScreen;
@@ -60,6 +75,7 @@ public class CarrierLabel extends SettingsPreferenceFragment implements
     private SwitchPreference mHideLabel;
     private ListPreference mNumberOfNotificationIcons;
     private ColorPickerPreference mColor;
+    private SeekBarPreference mStatusBarCarrierSize;
 
     private ContentResolver mResolver;
 
@@ -100,6 +116,12 @@ public class CarrierLabel extends SettingsPreferenceFragment implements
         mShowOnLockScreen = (SwitchPreference) findPreference(PREF_CARRIER_LABEL_SHOW_ON_LOCK_SCREEN);
         mShowOnLockScreen.setChecked(showOnLockScreen);
         mShowOnLockScreen.setOnPreferenceChangeListener(this);
+
+        mStatusBarCarrierSize =
+                (SeekBarPreference) findPreference(STATUS_BAR_CARRIER_FONT_SIZE);
+        mStatusBarCarrierSize.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14));
+        mStatusBarCarrierSize.setOnPreferenceChangeListener(this);
 
         if (!isHidden) {
             /*mUseCustom = (SwitchPreference) findPreference(PREF_CARRIER_LABEL_USE_CUSTOM);
@@ -155,8 +177,27 @@ public class CarrierLabel extends SettingsPreferenceFragment implements
             catColor.removePreference(findPreference(PREF_COLOR));
             removePreference("carrier_label_cat_notification_icons");
             removePreference("carrier_label_cat_color");
+            removePreference("status_bar_carrier_font_size");
         }
+        setHasOptionsMenu(true);
+    }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(com.android.internal.R.drawable.ic_settings_backup_restore)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -175,6 +216,11 @@ public class CarrierLabel extends SettingsPreferenceFragment implements
                     Settings.System.STATUS_BAR_CARRIER_LABEL_SHOW_ON_LOCK_SCREEN,
                     value ? 1 : 0);
             refreshSettings();
+            return true;
+         } else if (preference == mStatusBarCarrierSize) {
+            int width = ((Integer)newValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, width);
             return true;
         /*} else if (preference == mUseCustom) {
             value = (Boolean) newValue;
@@ -231,5 +277,67 @@ public class CarrierLabel extends SettingsPreferenceFragment implements
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.VRTOXIN_SHIT;
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        CarrierLabel getOwner() {
+            return (CarrierLabel) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.reset_android,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_CARRIER_LABEL_COLOR,
+                                    WHITE);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.reset_vrtoxin,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_CARRIER_LABEL_COLOR,
+                                    VRTOXIN_BLUE);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
     }
 }
