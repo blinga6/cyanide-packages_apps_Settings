da5a3051d8993b07c5a6742d21c02954871066d5
rogersb11 <brettrogers11@gmail.com>
2016-01-24 15:44:41 -0500

    Revert "CryptKeeper: pattern unlock displays incorrect pw when correct"
    This reverts commit cb90f1cd3ab5c3ac23ae2a37496ae357a3f5a41c.

--- src/com/android/settings/CryptKeeper.java
+++ src/com/android/settings/CryptKeeper.java
@@ -178,7 +178,6 @@ public class CryptKeeper extends Activity implements TextView.OnEditorActionList
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            mLockPatternView.removeCallbacks(mFakeUnlockAttemptRunnable);
             beginAttempt();
         }
 
