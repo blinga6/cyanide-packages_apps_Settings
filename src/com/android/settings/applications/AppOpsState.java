LineageOS/android_packages_apps_Settings

Settings: Convert AppOps array items to string references
* Avoid future breakage
 This will need conversion of all AOSP translated arrays to strings

Change-Id: I2e52e4013d1232d529ce5b9e3aae3502c42e938a

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries_cm);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_cm);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -97,7 +97,7 @@ public class AppOpsSummary extends Fragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_cm);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());

blinga6/cyanide-packages_apps_Settings

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(com.android.internal.R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries_vrtoxin);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_vrtoxin);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -106,7 +106,7 @@ public class AppOpsSummary extends InstrumentedFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_vrtoxin);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());