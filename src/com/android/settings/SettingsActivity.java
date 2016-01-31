mintkat/packages_apps_Settings

Settings: Privacy Guard [2/2]

src/com/android/settings/SettingsActivity.java
@@ -118,6 +118,7 @@ import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
+import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
@@ -284,7 +285,8 @@ public class SettingsActivity extends Activity
             R.id.home_settings,
             R.id.dashboard,
             R.id.bitsyko_layers,
-            R.id.main_settings
+            R.id.main_settings,
+            R.id.privacy_settings_cyanogenmod
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -362,9 +364,9 @@ public class SettingsActivity extends Activity
             WriteSettingsDetails.class.getName(),
             MainSettings.class.getName(),
             DisplayRotation.class.getName(),
-            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             BlacklistSettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName()
     };
 
 

src/com/android/settings/privacyguard/PrivacyGuardPrefs.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.privacyguard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "PrivacyGuardPrefs";
+
+    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
+    private CheckBoxPreference mPrivacyGuardDefault;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy_guard_prefs);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPrivacyGuardDefault = (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
+        mPrivacyGuardDefault.setOnPreferenceChangeListener(this);
+
+        mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.PRIVACY_GUARD_DEFAULT, 0) == 1);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        int paddingTop = list.getPaddingTop();
+        int paddingBottom = list.getPaddingBottom();
+        list.setPadding(0, paddingTop, 0, paddingBottom);
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPrivacyGuardDefault) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+}

blinga6/cyanide-packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -120,6 +120,7 @@ import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
+import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
@@ -296,7 +297,8 @@ public class SettingsActivity extends Activity
             R.id.about_settings,
             R.id.accessibility_settings,
             R.id.print_settings,
-            R.id.dashboard
+            R.id.dashboard,
+            R.id.privacy_settings_vrtoxin
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -392,7 +394,8 @@ public class SettingsActivity extends Activity
             ScreenPinningSettings.class.getName(),
             HeadsUpSettings.class.getName(),
             PowerMenuSettings.class.getName(),
-            Changelog.class.getName()
+            Changelog.class.getName(),
+            com.android.settings.vrtoxin.PrivacySettings.class.getName()
     };
 
 

src/com/android/settings/privacyguard/PrivacyGuardPrefs.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.privacyguard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "PrivacyGuardPrefs";
+
+    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
+    private CheckBoxPreference mPrivacyGuardDefault;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy_guard_prefs);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPrivacyGuardDefault = (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
+        mPrivacyGuardDefault.setOnPreferenceChangeListener(this);
+
+        mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.PRIVACY_GUARD_DEFAULT, 0) == 1);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        int paddingTop = list.getPaddingTop();
+        int paddingBottom = list.getPaddingBottom();
+        list.setPadding(0, paddingTop, 0, paddingBottom);
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPrivacyGuardDefault) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.VRTOXIN_AOSP;
+    }
+}