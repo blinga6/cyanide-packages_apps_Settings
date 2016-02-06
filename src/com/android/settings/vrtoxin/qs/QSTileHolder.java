f69d2e1f33c4eaa26da5dec83e1d38f0ccb43008
rogersb11 <brettrogers11@gmail.com>
2016-02-05 21:05:55 -0500

    Revert "Hide updater tile, meant to do this before, will bring back once builders are using ota setup, probably next release"
    This reverts commit e56b0f3578c318a04bceb5a1cfa69552fe53b57d.

--- src/com/android/settings/vrtoxin/qs/QSTileHolder.java
+++ src/com/android/settings/vrtoxin/qs/QSTileHolder.java
@@ -222,10 +222,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_powermenu";
                 stringId = R.string.qs_tile_powermenu;
                 break;
-            /*case QSConstants.TILE_VRTUPDATER:
+            case QSConstants.TILE_VRTUPDATER:
                 resourceName = "ic_qs_vrtoxin";
                 stringId = R.string.qs_tile_vrtupdater;
-                break;*/
+                break;
             case QSConstants.TILE_SOUNDS:
                 resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_tile_sounds;
