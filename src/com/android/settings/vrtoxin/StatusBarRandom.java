dba6a7b836c5d3587dcf3b612dfc50bb83551901
winga45 <winga45@gmail.com>
2016-06-06 16:43:02 -0400

    Revert "opps"
    This reverts commit f73ea7c2e6c3c3ac72218e7cb135eba60c2ca80a.

--- src/com/android/settings/vrtoxin/StatusBarRandom.java
+++ src/com/android/settings/vrtoxin/StatusBarRandom.java
@@ -17,16 +17,7 @@
 package com.android.settings.vrtoxin;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.content.Context;
-import android.content.res.Resources;
-import android.net.ConnectivityManager;
-import android.content.ContentResolver;
 import android.preference.ListPreference;
-import android.preference.SwitchPreference;
-import android.preference.PreferenceFragment;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
@@ -34,47 +25,18 @@ import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
 import com.android.settings.R;
-import com.android.settings.Utils;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
-import com.android.internal.util.vrtoxin.RRUtils;
- 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 public class StatusBarRandom extends SettingsPreferenceFragment {
 
-
-    private static final String SHOW_THREEG = "show_threeg";	
-
-    private SwitchPreference mShowThreeG;	
-    private boolean mCheckPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.vrtoxin_random);
-        PreferenceScreen prefSet = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver(); 
-        Context context = getActivity();
     }
-
-	mShowThreeG = (SwitchPreference) findPreference(SHOW_THREEG);
-        if (RRUtils.isWifiOnly(getActivity())) {
-            prefSet.removePreference(mShowThreeG);
-        } else {
-        mShowThreeG.setChecked((Settings.System.getInt(resolver,
-                Settings.System.SHOW_THREEG, 0) == 1));
-        }
-        
-        setHasOptionsMenu(true);
-        mCheckPreferences = true;
-        return prefSet;
-    }
-
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
@@ -83,16 +45,10 @@ public class StatusBarRandom extends SettingsPreferenceFragment {
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
-         if  (preference == mShowThreeG) {
-            boolean checked = ((SwitchPreference)preference).isChecked();
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SHOW_THREEG, checked ? 1:0);
-            return true;
+ 		return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
-       return super.onPreferenceTreeClick(preferenceScreen, preference);
-}
- 
-   @Override
+
+    @Override
     protected int getMetricsCategory() {
         return MetricsLogger.VRTOXIN_SHIT;
     }
